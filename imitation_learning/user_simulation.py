b1cd2f2569a155ba66a278e97c34cf97fd87b532
Shobhit Chaurasia <sc.shobhit@gmail.com>
2016-11-27 18:13:14 -0600

    Fix documentation

--- imitation_learning/user_simulation.py
+++ imitation_learning/user_simulation.py
@@ -10,18 +10,8 @@ class UserSimulation(object):
             `Reward` function that gave rise to this user-simulation.
     """
 
-    def __init__(self, policy, q, weights, distance):
-        """Constructor for UserSimulation class
-
-        Args:
-            policy (:obj: UserPolicy): Policy defining the user simulation
-            q (dict): Q-values of the policy.
-            weights (1D numpy.ndarray): The weight vectors characterizing the
-                `Reward` function that gave rise to this user-simulation.
-            distance (float): Eucledian distance between the feature
-                expectations of the expert user and the simulated user.
-        """
+    def __init__(self, policy, q, weights, distance_to_expert):
         self.policy = policy
         self.q = q
         self.weights = weights
-        self.distance_to_expert = distance
+        self.distance_to_expert = distance_to_expert
